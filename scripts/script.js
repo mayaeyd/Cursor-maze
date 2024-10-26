58e536f15c7e767d11ff9b52a9801b7c8555a27d
Maya Eid <mayabaseleid@gmail.com>
2024-10-27 01:56:05 +0200

    Find initial offset on mouse enter

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,22 @@
-// y start button min: 330 max : 360
-// x start button min: 140 max : 180
+// // // y start button min: 330 max : 360
+// // // x start button min: 140 max : 180
 
 var startBtn= document.getElementById("start");
 var cursor;
-var offset=[0,0];
+offset=[0,0]
+
+startBtn.addEventListener("mouseenter",(e)=>{
+    offset=[
+        startBtn.offsetLeft-e.clientX,
+        startBtn.offsetTop-e.clientY
+    ]
+})
 
 startBtn.addEventListener("mousemove",(event)=>{
     document.getElementById("status").innerHTML="Try not to hit the barriers!";
 
-    cursor={
-        x: event.clientX,
-        y: event.clientY,
-    }
-
     
+
 })
+
+
